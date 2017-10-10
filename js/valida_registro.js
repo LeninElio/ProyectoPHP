3d04ea2ca597832de7010b002711467fe514b340
Lenin Elio <moreno_leninelio@hotmail.es>
2017-10-10 11:23:27 -0500

    Add files via upload

---
+++ js/valida_registro.js
@@ -0,0 +1,40 @@
+with(document.registro) {
+    onsubmit = function (e) {
+        e.preventDefault()
+        ok = true;
+        if (ok && username.value=="") {
+            ok = false;
+                alert("Debe de escribir un nombre de usuario");
+            username.focus();
+        }
+        if (ok && fullname.value=="") {
+            ok = false;
+                alert("Debe de escribir un nombre");
+            fullname.focus();
+        }
+        if (ok && email.value=="") {
+            ok = false;
+                alert("Debe de escribir un correo");
+            email.focus();
+        }
+        if (ok && password.value=="") {
+            ok = false;
+                alert("Debe de escribir una contraseña");
+            password.focus();
+        }
+        if (ok && confirm_password.value=="") {
+            ok = false;
+                alert("Debe de escribir una contraseña de confirmacion");
+            confirm_password.focus();
+        }
+        if (ok && password.value!=confirm_password.value) {
+            ok = false;
+                alert("las contraseñas no coinciden");
+            confirm_password.focus();
+        }
+        
+        if(ok){
+            submit();
+        }
+    }
+}
